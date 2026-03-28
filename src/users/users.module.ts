ca13dc38735044208a588842c1ade47a6e0fb4f6
Daler <isoev9733@gmail.com>
2026-03-28 22:54:40 +0500

    feat: add users service, mutations and input types

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersResolver } from './users.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [UsersResolver],
+  providers: [UsersResolver, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
